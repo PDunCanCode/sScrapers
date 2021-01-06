0081b91dbd863a4abf2799086b1754d42628a649
pDunCanCode <chesiredarling@gmail.com>
2021-01-05 21:05:13 -0800

    decorators

--- typescriptNotes/app.ts
+++ typescriptNotes/app.ts
@@ -5,7 +5,18 @@ function Logger(logString: string) {
   };
 }
 
-@Logger("Logging - Human")
+function WithTemplate(template: string, hookId: string) {
+  return function (_: Function) {
+    const hookEl = document.getElementById(hookId);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+    }
+  };
+}
+
+//@Logger("Logging - Human")
+
+@WithTemplate("<h1> My Human Object</h1>", "app")
 class Human {
   name = "Paul";
 
@@ -17,3 +28,48 @@ class Human {
 const pers = new Human();
 
 console.log(pers);
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property Decorator");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price");
+    }
+  }
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+  @Log3
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
