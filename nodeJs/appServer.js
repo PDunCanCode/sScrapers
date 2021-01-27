993981532e0a29b85df78dbe8f5e334a3d4d8ef6
pDunCanCode <chesiredarling@gmail.com>
2021-01-27 13:38:51 -0800

    add raw server setup

---
+++ nodeJs/appServer.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Messgae</title></head>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    rs.setHeader("Location", "/");
+    return res.end();
+  }
+});
+
+server.listen(3000);
