f0ab92be435a9ab3c336bcd3159fa516624b8ce6
pDunCanCode <chesiredarling@gmail.com>
2021-01-26 21:24:37 -0800

    'quicksort'

---
+++ JSAlgos/quickSort.js
@@ -0,0 +1,34 @@
+function quickSort(arr) {
+  const copiedArray = [...arr];
+
+  if (copiedArray.length <= 1) {
+    return copiedArray;
+  }
+
+  const smallerElementsArray = [];
+  const biggerElementsArray = [];
+  const pivotElement = copiedArray.shift();
+  const centerElementsArray = [pivotElement];
+
+  while (copiedArray.length) {
+    const currentElement = copiedArray.shift();
+    if (currentElement === pivotElement) {
+      centerElementsArray.push(currentElement);
+    } else if (currentElement < pivotElement) {
+      smallerElementsArray.push(currentElement);
+    } else {
+      biggerElementsArray.push(currentElement);
+    }
+  }
+
+  const smallerElementsSortedArray = quickSort(smallerElementsArray);
+  const biggerElementsSortedArray = quicksort(biggerElementsArray);
+
+  return smallerElementsSortedArray.concat(
+    centerElementsArray,
+    biggerElementsSortedArray
+  );
+}
+
+const sortedArray = quickSort([-3, 10, 1, 100, -10, 22, 15]);
+console.log(sortedArray);
